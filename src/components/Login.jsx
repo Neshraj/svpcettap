5e233627eb44085afa84b8b91ef16f52d2dd5a35
Neshraj <sneshrajappu@gmail.com>
2024-10-11 01:56:18 +0530

    Removed wast

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 function Login(){
 
     const navigate = useNavigate();
-    const mainServerUrl ="https://svpcettapserver.onrender.com/";
+    const mainServerUrl ="https://svpcettapserver-raj1.onrender.com/";
 
     function togglePasswordVisibility() {
         var passwordInput = document.getElementById("password");
